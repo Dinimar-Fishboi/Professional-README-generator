9b30bab3d1e966b48cedf79e8f10a1357bad8f26
Dinimar Fish <tayla@tallisman.com.au>
2021-08-04 20:19:38 +1000

    updated questions array and placed packages required for application

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const autoProfile = require('./autoProfile')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+      },
+{
+  type: 'input',
+  message: 'Please enter your Git URL',
+  name: 'GitHub',
+},
+{
+  type: 'input',
+  message: 'Please enter you LinkedIn URL',
+  name: 'LinkedIn',
+},
+{
+  type: 'input',
+  message: 'This is the About me Section',
+  name: 'Bio',
+},
+{
+  type: 'input',
+  message: 'Where are you from?',
+  name: 'Location',
+},
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
